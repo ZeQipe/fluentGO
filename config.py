46be51572eaa284190d66ef52ec307134d0e2e9c
ZeQipe <zeqipe@vk.com>
2025-11-22 16:27:17 +0300

    Enhance payment system labels with icons in configuration
    - Updated the payment system labels in `config.py` for one-time and subscription payments to include icons for better visual representation.
    - Adjusted the default payment system label to maintain consistency with the new format.

--- config.py
+++ config.py
@@ -7,18 +7,18 @@ MODAL_LANGUAGES = ["ru"]
 
 # Платежные системы для разовых покупок (type = "one-time")
 ONE_TIME_PAYMENT_SYSTEMS = [
-    {"label": "Яндекс Пэй", "key": "yandex_pay"},
-    {"label": "PayPal", "key": "paypal"}
+    {"label": "💳Яндекс Пэй", "key": "yandex_pay"},
+    {"label": "💲PayPal", "key": "paypal"}
 ]
 
 # Платежные системы для подписок (type = "subscription")
 SUBSCRIPTION_PAYMENT_SYSTEMS = [
-    {"label": "ЮКасса", "key": "yookassa"},
-    {"label": "PayPal", "key": "paypal"}
+    {"label": "💳ЮКасса", "key": "yookassa"},
+    {"label": "💲PayPal", "key": "paypal"}
 ]
 
 # Платежная система по умолчанию (когда hasModal = False)
 DEFAULT_PAYMENT_SYSTEM = [
-    {"label": "PayPal", "key": "paypal"}
+    {"label": "💲PayPal", "key": "paypal"}
 ]
 
